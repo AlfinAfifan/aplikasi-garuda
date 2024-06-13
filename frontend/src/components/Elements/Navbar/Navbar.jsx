1a1c53cfe6b07fdd3bf3f66b0996f3b6fbdc9720
AlfinAfifan <agusalfin1@gmail.com>
2024-06-13 10:49:14 +0700

    delete search in navbar

--- frontend/src/components/Elements/Navbar/Navbar.jsx
+++ frontend/src/components/Elements/Navbar/Navbar.jsx
@@ -25,18 +25,6 @@ const Navbar = () => {
   const name = sessionStorage.getItem("name")
   return (
     <div className="sticky top-0 z-20 flex h-14 items-center justify-end bg-white px-5 shadow-md font-montserrat">
-      {/* <div className="flex w-[35%] items-center">
-        <input
-          type="text"
-          className="h-8 w-full rounded-l-md border border-gray-500 p-2"
-        />
-        <button
-          type="submit"
-          className="h-8 rounded-r-md bg-second px-2 font-semibold text-white hover:bg-third"
-        >
-          <MagnifyingGlassIcon className="w-5" />
-        </button>
-      </div> */}
       <div
         className="flex cursor-pointer items-center gap-2"
         onClick={handleLogout}
